9b5d4f8eca24487ef1e06164740803284f204bff
Gabriel Costa <gabrielcerqueira96@hotmail.com>
2020-11-03 22:05:09 -0300

    fix: add new controllers singletons to di container
    add new controllers to di container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,12 @@
 import mikroConfig from '@config/mikro.config';
 import { DepartamentController } from '@modules/departaments/ui/departament.controller';
+import { EmployeeController } from '@modules/employees/ui/employee.controller';
+import { SupplierController } from '@modules/supplier/ui/supplier.controller';
 import { BaseController } from '@shared/core/baseController';
 import { MikroOrmClient } from '@shared/infra/orm';
 import { container } from 'tsyringe'
 
 container.registerInstance("OrmConfig", mikroConfig);
 container.registerSingleton<BaseController>("Controllers",DepartamentController)
+container.registerSingleton<BaseController>("Controllers",EmployeeController)
+container.registerSingleton<BaseController>("Controllers",SupplierController)
