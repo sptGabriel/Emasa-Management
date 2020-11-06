711d6776a37a35b076d5b57a4efbc6788385b542
gabrielti <gabrielti@emasa.local>
2020-11-06 13:19:27 -0300

    fix(tsyringe): contract singleton registry
    contract singleton registry

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,15 +1,20 @@
 import mikroConfig from '@config/mikro.config';
+import { ContractController } from '@modules/contracts/ui/contract.controller';
 import { DepartamentController } from '@modules/departaments/ui/departament.controller';
 import { EmployeeController } from '@modules/employees/ui/employee.controller';
 import { ProductsController } from '@modules/products/ui/products.controller';
 import { SupplierController } from '@modules/supplier/ui/supplier.controller';
 import { SuppliesController } from '@modules/supplying/ui/supplies.controller';
 import { BaseController } from '@shared/core/baseController';
-import { container } from 'tsyringe'
+import { container } from 'tsyringe';
 
-container.registerInstance("OrmConfig", mikroConfig);
-container.registerSingleton<BaseController>("Controllers",DepartamentController)
-container.registerSingleton<BaseController>("Controllers",EmployeeController)
-container.registerSingleton<BaseController>("Controllers",SupplierController)
-container.registerSingleton<BaseController>("Controllers",ProductsController)
-container.registerSingleton<BaseController>("Controllers",SuppliesController)
+container.registerInstance('OrmConfig', mikroConfig);
+container.registerSingleton<BaseController>(
+  'Controllers',
+  DepartamentController,
+);
+container.registerSingleton<BaseController>('Controllers', EmployeeController);
+container.registerSingleton<BaseController>('Controllers', SupplierController);
+container.registerSingleton<BaseController>('Controllers', ProductsController);
+container.registerSingleton<BaseController>('Controllers', SuppliesController);
+container.registerSingleton<BaseController>('Controllers', ContractController);
