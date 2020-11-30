9477ac9b92f2f4a1d0bdb6ac521f8c8abb9bfeb5
unknown <you@example.com>
2020-11-29 23:28:31 -0300

    fix(react): working with mobx
    working with mobx

--- packages/client/src/store/rootStore.ts
+++
@@ -1,8 +0,0 @@
-import { types } from 'mobx-state-tree';
-import { Employee } from '../models/employee';
-import { UserModel } from '../models/user';
-
-export const RootStore = types.model({
-  employees: types.map(Employee),
-  user: types.optional(types.map(UserModel), {})
-});
