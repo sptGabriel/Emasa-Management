e882f4f51db8ffafc33a340f8092ba074387337c
gabriel <gabrielcerqueira96@hotmail.com>
2021-02-18 13:28:11 -0300

    fix(any): aniy
    any

--- packages/client/src/shared/components/Export.tsx
+++ packages/client/src/shared/components/Export.tsx
@@ -1,4 +1,5 @@
-import React, {useState} from 'react'
+import React, {useRef, useState} from 'react'
+import useOnClickOutside from 'use-onclickoutside'
 import Modal from './Modal'
 
 const PeriodOption: React.FC<{state: any; setState: any}> = ({
@@ -322,8 +323,116 @@ const PeriodOption: React.FC<{state: any; setState: any}> = ({
   )
 }
 
-const ColumnsNames: React.FC<{state: any; setState: any}> = () => {
-  return <div>a</div>
+const ColumnsNames: React.FC<{state: any; setState: any}> = ({
+  setState,
+  state,
+}) => {
+  const [isOpen, setOpen] = useState(false)
+  const ref: any = useRef()
+  useOnClickOutside(ref, () => {
+    setOpen(false)
+  })
+
+  function clickHandler(name: any) {
+    if (!name) return
+    setState(
+      state.map((item: any) => ({
+        ...item,
+        active: item.name === name || false,
+      })),
+    )
+    setOpen(false)
+  }
+
+  function renderOptions() {
+    return (
+      <div className="absolute mt-1 rounded-md bg-white shadow-lg">
+        <ul
+          style={{width: '200px'}}
+          ref={ref}
+          role="listbox"
+          className="max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm"
+        >
+          {state.map((item: any, index: number) => (
+            <li
+              key={index}
+              id="listbox-item-0"
+              className="cursor-pointer pl-4 pr-10 text-gray-900 cursor-default select-none relative py-2"
+            >
+              <button
+                className="w-full flex"
+                type="button"
+                onClick={() => clickHandler(item.name)}
+              >
+                <span className="block font-normal truncate">{item.name}</span>
+              </button>
+              {item.active ? (
+                <span className="absolute inset-y-0 right-0 flex items-center pr-4">
+                  <svg
+                    className="h-5 w-5"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </span>
+              ) : (
+                ''
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
+  function renderCustomOptions() {
+    return (
+      <div>
+        <p>a</p>
+      </div>
+    )
+  }
+
+  return (
+    <div className="mt-1">
+      <button
+        style={{width: '200px'}}
+        onClick={() => setOpen(!isOpen)}
+        type="button"
+        className="relative bg-white border border-gray-300 rounded-md shadow-sm pl-4 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+      >
+        <span className="block truncate">
+          {state.find((item: any) => item.active).name}
+        </span>
+        <span className="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+          <svg
+            className="h-5 w-5 text-gray-400"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </span>
+      </button>
+      {isOpen ? renderOptions() : ''}
+      {state.find((item: any) => item.name === 'Personalizado' && item.active)
+        ? renderCustomOptions()
+        : ''}
+    </div>
+  )
 }
 
 export const ExportModal: React.FC<{
@@ -348,7 +457,11 @@ export const ExportModal: React.FC<{
     },
   })
 
-  const [columnNames, setColumns] = useState({})
+  const [columnNames, setColumns] = useState([
+    {active: true, columns, name: `Padrão (${columns.length})`},
+    {active: false, columns: [], name: `Todas as Colunas (${columns.length})`},
+    {active: false, columns, name: `Personalizado`},
+  ])
 
   return (
     <Modal
@@ -367,8 +480,9 @@ export const ExportModal: React.FC<{
             <span className="font-medium text-sm">Selecione um período</span>
             <PeriodOption setState={setType} state={exportType} />
           </div>
-          <div>
+          <div className="max-w-md">
             <span className="font-medium text-sm">Selecione as colunas</span>
+            <ColumnsNames state={columnNames} setState={setColumns} />
           </div>
         </div>
         <footer
--- packages/client/src/shared/components/Modal.tsx
+++ packages/client/src/shared/components/Modal.tsx
@@ -6,75 +6,6 @@ import useOnClickOutside from 'use-onclickoutside'
 
 const modal = document.getElementById('emasa-modal')
 
-const ModalWrap = styled('div')<{isShowing: boolean}>`
-  position: relative;
-  width: 100%;
-  max-width: 768px;
-  max-height: 90vh;
-  padding: 33px;
-  background: white;
-  opacity: ${({isShowing}) => (isShowing ? '1' : '0')};
-  border-radius: 5px;
-  transform: ${({isShowing}) =>
-    isShowing ? 'translateY(0px)' : 'translateY(20px)'};
-  transition: transform 0.2s ease-in 0s, opacity 0.2s ease-in 0s;
-  overflow-y: auto;
-  text-align: left;
-  cursor: default;
-  .tittle {
-    font-size: 24px;
-    font-weight: bold;
-    font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
-    margin-bottom: 0.5em;
-    color: #000;
-  }
-  .close-bt {
-    position: absolute;
-    color: #000;
-    right: 15px;
-    top: 10px;
-    cursor: pointer;
-  }
-`
-const EmasaModal = styled('div')<{isShowing: boolean}>`
-  position: fixed;
-  display: flex;
-  width: 100%;
-  height: 100%;
-  top: 0px;
-  left: 0px;
-  background: rgba(0, 0, 0, 0.2);
-  z-index: 9999;
-  -webkit-box-align: center;
-  align-items: center;
-  -webkit-box-pack: center;
-  justify-content: center;
-  visibility: ${({isShowing}) => (isShowing ? 'visible' : 'hidden')};
-  opacity: ${({isShowing}) => (isShowing ? '1' : '0')};
-  transition: all 0.3s ease 0s;
-  cursor: pointer;
-`
-const ModalContainer = styled('div')<{isShowing: boolean}>`
-  position: fixed;
-  display: flex;
-  -webkit-box-align: center;
-  align-items: center;
-  -webkit-box-pack: center;
-  justify-content: center;
-  width: 100%;
-  height: 100%;
-  top: 0px;
-  left: 0px;
-  padding: 30px;
-  background: rgba(0, 0, 0, 0.365);
-  color: rgb(255, 255, 255);
-  z-index: 9998;
-  font-size: 16px;
-  opacity: ${({isShowing}) => (isShowing ? '1' : '0')};
-  visibility: ${({isShowing}) => (isShowing ? 'visible' : 'hidden')};
-  transition: all 0.3s ease 0s;
-`
-
 const Modal = ({isShowing, hide, hideWithOutSide, children, tittle}: any) => {
   const ref: any = useRef()
   useOnClickOutside(ref, () => {
@@ -113,7 +44,7 @@ const Modal = ({isShowing, hide, hideWithOutSide, children, tittle}: any) => {
                 margin: '0 auto',
                 maxHeight: 'calc(100vh - 80px)',
               }}
-              className="w-full col-start-2 rounded-md shadow-md overflow-hidden bg-white flex flex-col relative"
+              className="w-full col-start-2 rounded-md shadow-md overflow-hidden bg-white flex flex-col"
             >
               <div
                 style={{boxShadow: 'inset 0 -1px #e3e8ee'}}
